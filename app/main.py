6fa53763597055b725ca5970147f703a10f6fe83
david <david.scott.2875@gmail.com>
2025-11-26 10:15:06 +0100

    Fix lint: ignore B008 and shorten long line

--- app/main.py
+++ app/main.py
@@ -39,4 +39,7 @@ def health():
 secret = "fezffzefzefzlfzhfzfzfjzfzfzfdzgerg54g651fzefg51zeg5g"
 API_KEY = "sk-1234567890abcdef"
 
-very_long_variable_name_that_exceeds_line_length = "Cette ligne est intentionnellement trop longue pour violer les règles de formatage standard"
+message = (
+    "Cette ligne est intentionnellement trop longue "
+    "pour violer les règles de formatage standard"
+)
